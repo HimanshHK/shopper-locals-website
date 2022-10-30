6feab2a85dc8037fb9c94640fab03dd00838da40
sachin492002 <82491840+sachin492002@users.noreply.github.com>
2022-10-30 16:12:23 +0530

    Json Server

--- src/pages/Feeds.js
+++ src/pages/Feeds.js
@@ -1,57 +1,35 @@
-import React, {useEffect, useReducer, useState} from 'react';
+import React, {useEffect,  useState} from 'react';
 import styled from 'styled-components';
 import 'react-icons/fa'
 import axios from "axios";
 import {FaUserCircle} from "react-icons/all";
 
 
-const reducer = (state, action) => {
-    switch (action.type) {
-        case 'FETCH_REQUEST':
-            return { ...state, loading: true };
-        case 'FETCH_SUCCESS':
-            return { ...state, product: action.payload, loading: false };
-        case 'FETCH_FAIL':
-            return { ...state, loading: false, error: action.payload };
-        default:
-            return false;
-    }
-};
-
 
 const Feeds = () => {
 
-    const [{ loading, error, product }, dispatch] = useReducer(reducer, {
-        product:[],
-        loading: true,
-        error: '',
-    });
-    useEffect(() => {
-        const fetchData = async () => {
-            dispatch({ type: 'FETCH_REQUEST' });
-            try {
-                const result = await axios.get('http://localhost:3001/feeds');
-                dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
-            } catch (err) {
-                dispatch({ type: 'FETCH_FAIL', payload: err.message });
-            }
 
-        };
-        fetchData();
-    }, []);
+    const [data,setData]=useState([]);
+    useEffect(()=>{
+        axios.get('http://localhost:3001/feeds')
+            .then(response => {
+                console.log(response)
+                setData(response.data)
+            })
+    })
     return(
         <Wrapper>
             <div className='test1'><h1>What users say about us!
 
             </h1></div>
             <div className="testimonial-box-container">
-            {loading ? (
-                <div>Loading...</div>
-            ) : error ? (
-                <div>{error}</div>
-            ) : (
-                product.map((msg) => (
-
+            {/*{loading ? (*/}
+            {/*    <div>Loading...</div>*/}
+            {/*) : error ? (*/}
+            {/*    <div>{error}</div>*/}
+            {/*) : (*/}
+                { (
+                data.map((msg) => (
                         <div className="testimonial-box">
                             <div className="box-top">
                                 <div className="profile">
